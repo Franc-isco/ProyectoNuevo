e42e56654474d0553a52f18c6ebfb188f42e06a4
Francisco Tabilo <francisco.tabilo05@inacapmail.cl>
2024-12-09 00:54:37 -0300

    get

--- auxiliares/url_servicio.py
+++ auxiliares/url_servicio.py
@@ -1,1 +1,1 @@
-url_base_jsonplaceholder = "https://jsonplaceholder.typicode.com"
+url_base_jsonplaceholder = "https://jsonplaceholder.typicode.com/"
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import servicios.consumir_api
 
-#servicios.consumir_api.consulta_api_todos()
+servicios.consumir_api.consulta_api_todos()
 
 #servicios.consumir_api.consumir_api_users()
